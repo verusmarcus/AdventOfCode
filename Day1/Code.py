b5f4205cf510aadfafd843a93d9f05375d43a350
Yannick <ymeinerzhagen@web.de>
2023-12-25 08:01:58 +0100

    added solution for part two

--- Day1/Code.py
+++ Day1/Code.py
@@ -13,16 +13,26 @@ def read_file(filename):
 
 
 def find_first_number(line):
-    for character in line:
+    first_letter_list = ['o', 't', 'f', 's', 'e', 'n']
+    for i, character in enumerate(line):
         if character.isdigit():
             return character
+        if character in first_letter_list:
+            number = check_spelled_letter(line + 'xxxxx', character, i)
+            if number:
+                return number
     return '0'
 
 
 def find_last_number(line):
+    first_letter_list = ['o', 't', 'f', 's', 'e', 'n']
     for i in range(len(line)-1, -1, -1):
         if line[i].isdigit():
             return line[i]
+        if line[i] in first_letter_list:
+            number = check_spelled_letter(line + 'xxxxx', line[i], i)
+            if number:
+                return number
     return '0'
 
 
@@ -55,6 +65,39 @@ def get_all_numbers(data):
 def sum_list_values(values):
     total_value = sum(values)
     return total_value
+
+
+def check_spelled_letter(line, character, position):
+    if character == 'o':
+        if check_string_for_number(line[position:position+3], 'one'):
+            return '1'
+    elif character == 't':
+        if check_string_for_number(line[position:position+3], 'two'):
+            return '2'
+        if check_string_for_number(line[position:position+5], 'three'):
+            return '3'
+    elif character == 'f':
+        if check_string_for_number(line[position:position+4], 'four'):
+            return '4'
+        if check_string_for_number(line[position:position+4], 'five'):
+            return '5'
+    elif character == 's':
+        if check_string_for_number(line[position:position+3], 'six'):
+            return '6'
+        if check_string_for_number(line[position:position+5], 'seven'):
+            return '7'
+    elif character == 'e':
+        if check_string_for_number(line[position:position+5], 'eight'):
+            return '8'
+    elif character == 'n':
+        if check_string_for_number(line[position:position+4], 'nine'):
+            return '9'
+    else:
+        return False
+
+
+def check_string_for_number(line, text):
+    return line == text
 
 
 main()
--- Day1/Tests.py
+++ Day1/Tests.py
@@ -3,6 +3,7 @@ import Code
 
 TEST_FILENAME = 'Testdata.txt'
 TEST_DATA = Code.read_file(TEST_FILENAME)
+TEST_DATA_2 = Code.read_file('Test2.txt')
 
 
 # empty test
@@ -22,11 +23,15 @@ def test_read_file():
 def test_find_first_number():
     number = Code.find_first_number(TEST_DATA[0])
     assert number == '1'
+    number = Code.find_first_number(TEST_DATA_2[0])
+    assert number == '2'
 
 
 def test_find_last_number():
     number = Code.find_last_number(TEST_DATA[0])
     assert number == '2'
+    number = Code.find_last_number(TEST_DATA_2[0])
+    assert number == '9'
 
 
 def test_combine_numbers():
@@ -44,11 +49,15 @@ def test_get_number():
     assert number == 12
     number = Code.get_number('abc')
     assert number == 0
+    number = Code.get_number(TEST_DATA_2[0])
+    assert number == 29
 
 
 def test_get_all_numbers():
     number_list = Code.get_all_numbers(TEST_DATA)
     assert number_list == [12, 38, 15, 77]
+    number_list = Code.get_all_numbers(TEST_DATA_2)
+    assert number_list == [29, 83, 13, 24, 42, 14, 76]
 
 
 def test_sum_list_values():
